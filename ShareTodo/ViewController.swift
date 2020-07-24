1a15868f64238287cae24e41777192a6fdb1cc48
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-07-24 14:25:12 +0900

    Initial Commit

---
+++ ShareTodo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShareTodo
+//
+//  Created by jun on 2020/07/24.
+//  Copyright © 2020 jun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

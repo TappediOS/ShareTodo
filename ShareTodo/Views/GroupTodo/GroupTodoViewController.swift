8457acf798026eae89be9ffbbbdea980ed747617
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-07-24 16:38:10 +0900

    tableviewにcellの情報を表示する処理を書いた

--- ShareTodo/Views/GroupTodo/GroupTodoViewController.swift
+++ ShareTodo/Views/GroupTodo/GroupTodoViewController.swift
@@ -13,9 +13,20 @@ final class GroupTodoViewController: UIViewController {
     
     @IBOutlet weak var groupTableView: UITableView!
     
+    var todo = ["test", "test2"]
+    private let groupTodoCellID = "GroupTableViewCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.setupGroupTableView()
+    }
+    
+    func setupGroupTableView() {
+        self.groupTableView.rowHeight = 85
+        self.groupTableView.delegate = self
+        self.groupTableView.dataSource = self
+        self.groupTableView.tableFooterView = UIView()
     }
     
     func inject(with presenter: GroupTodoViewPresenterProtocol) {
@@ -26,4 +37,26 @@ final class GroupTodoViewController: UIViewController {
 
 extension GroupTodoViewController: GroupTodoViewPresenterOutput {
     
+}
+
+extension GroupTodoViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.todo.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = self.groupTableView.dequeueReusableCell(withIdentifier: self.groupTodoCellID, for: indexPath)
+                         as? GroupTableViewCell else { return UITableViewCell() }
+        
+        cell.groupNameLabel.text = "Test"
+        cell.groupMembersNameLabel.text = "list, bent, run, aws"
+        cell.groupImageView.image = UIImage(systemName: "paperclip.circle.fill")
+        cell.groupImageView.tintColor = .systemGreen
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.groupTableView.deselectRow(at: indexPath, animated: true)
+    }
 }

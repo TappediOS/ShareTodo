af525b1f923808bfd65de936bdfd951d76dcdd3c
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-07-24 16:28:50 +0900

    GroupTableViewCellファイルを追加

---
+++ ShareTodo/Views/GroupTodo/GroupTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  GroupTableViewCell.swift
+//  ShareTodo
+//
+//  Created by jun on 2020/07/24.
+//  Copyright © 2020 jun. All rights reserved.
+//
+
+import UIKit
+
+class GroupTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+}
+

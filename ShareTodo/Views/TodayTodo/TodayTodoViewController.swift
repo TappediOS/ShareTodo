b226a2e89823f51bddc942e1d12ebbe40ef87134
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-07-24 15:41:21 +0900

    TodayTodoViewの各種ファイルを追加

---
+++ ShareTodo/Views/TodayTodo/TodayTodoViewController.swift
@@ -0,0 +1,26 @@
+//
+//  TodayTodoViewController.swift
+//  ShareTodo
+//
+//  Created by jun on 2020/07/24.
+//  Copyright © 2020 jun. All rights reserved.
+//
+
+import UIKit
+
+final class TodayTodoViewController: UIViewController {
+    private var presenter: TodayTodoViewPresenterProtocol!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func inject(with presenter: TodayTodoViewPresenterProtocol) {
+        self.presenter = presenter
+        self.presenter.view = self
+    }
+}
+
+extension TodayTodoViewController: TodayTodoViewPresenterOutput {
+    
+}

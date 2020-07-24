6101405775f5298faf191f76ca5608831387c781
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-07-24 17:22:18 +0900

    collectionviewの中身を表示させた

--- ShareTodo/Views/TodayTodo/TodayTodoCollectionViewCell.swift
+++ ShareTodo/Views/TodayTodo/TodayTodoCollectionViewCell.swift
@@ -9,10 +9,30 @@
 import UIKit
 
 class TodayTodoCollectionViewCell: UICollectionViewCell {
-        
+    @IBOutlet weak var groupImageView: UIImageView!
+    @IBOutlet weak var taskLabel: UILabel!
+    @IBOutlet weak var radioButton: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-
+        
+        self.setupGroupImageView()
+        self.setupTaskLabel()
+        self.setupRadioButton()
     }
-
+    
+    func setupGroupImageView() {
+        self.groupImageView.layer.cornerRadius = self.groupImageView.frame.width / 2
+        self.groupImageView.layer.masksToBounds = true
+    }
+    
+    func setupTaskLabel() {
+        self.taskLabel.adjustsFontSizeToFitWidth = true
+        self.taskLabel.minimumScaleFactor = 0.4
+    }
+    
+    func setupRadioButton() {
+        self.radioButton.layer.cornerRadius = self.groupImageView.frame.width / 2
+        self.radioButton.layer.masksToBounds = true
+    }
 }
--- ShareTodo/Views/TodayTodo/TodayTodoViewController.swift
+++ ShareTodo/Views/TodayTodo/TodayTodoViewController.swift
@@ -14,8 +14,18 @@ final class TodayTodoViewController: UIViewController {
     
     private let todayTodoCollectionViewCellId = "TodayTodoCollectionViewCell"
     
+    var todo = ["test", "lost", "cooked"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.setupTodayTodoCollectionView()
+    }
+    
+    func setupTodayTodoCollectionView() {
+        self.todayTodoCollectionView.collectionViewLayout.invalidateLayout()
+        self.todayTodoCollectionView.delegate = self
+        self.todayTodoCollectionView.dataSource = self
     }
     
     func inject(with presenter: TodayTodoViewPresenterProtocol) {
@@ -26,4 +36,34 @@ final class TodayTodoViewController: UIViewController {
 
 extension TodayTodoViewController: TodayTodoViewPresenterOutput {
     
+}
+
+extension TodayTodoViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.todo.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: todayTodoCollectionViewCellId, for: indexPath) as! TodayTodoCollectionViewCell
+
+        //TODO:- 実際の文字列を表示すること
+        cell.taskLabel.text = "朝起きをすること"
+        cell.groupImageView.image = UIImage(systemName: "cloud.sun.rain.fill")
+        cell.groupImageView.tintColor = .systemTeal
+   
+        return cell
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        let width = UIScreen.main.bounds.width
+        return CGSize(width: width, height: 70)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 10, left: 0, bottom: 0, right: 0)
+    }
 }

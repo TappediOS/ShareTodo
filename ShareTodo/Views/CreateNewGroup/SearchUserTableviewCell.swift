fd0a87d95c21f2a7e07902874ee8ef9cd431351a
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-07-26 14:22:57 +0900

    部品を繋いでセットアップした

--- ShareTodo/Views/CreateNewGroup/SearchUserTableviewCell.swift
+++ ShareTodo/Views/CreateNewGroup/SearchUserTableviewCell.swift
@@ -9,9 +9,31 @@
 import UIKit
 
 class SearchUserTableviewCell: UITableViewCell {
+    @IBOutlet weak var radioImageView: UIImageView!
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
+        self.setupRadioImageView()
+        self.setupProfileImageView()
+        self.setupUserNameLabel()
+    }
+    
+    private func setupRadioImageView() {
+        self.radioImageView.contentMode = .scaleAspectFill
+        self.radioImageView.layer.cornerRadius = self.radioImageView.frame.width / 2
+        self.radioImageView.layer.masksToBounds = true
+    }
+    
+    private func setupProfileImageView() {
+        self.profileImageView.layer.cornerRadius = self.profileImageView.frame.width / 2
+        self.profileImageView.layer.masksToBounds = true
+    }
+    
+    private func setupUserNameLabel() {
+        self.userNameLabel.adjustsFontSizeToFitWidth = true
+        self.userNameLabel.minimumScaleFactor = 0.4
     }
 }
--- ShareTodo/Views/CreateNewGroup/SelectedUserCollectionViewCell.swift
+++ ShareTodo/Views/CreateNewGroup/SelectedUserCollectionViewCell.swift
@@ -9,9 +9,37 @@
 import UIKit
 
 class SelectedUserCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var deleteUserButton: UIButton!
     
+    var deleteUserButtonAction: (() -> Void)?
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        self.setupProfileImageView()
+        self.setupUserNameLabel()
+        self.setupDeleteUserButton()
+    }
+    
+    private func setupProfileImageView() {
+        self.profileImageView.layer.cornerRadius = self.profileImageView.frame.width / 2
+        self.profileImageView.layer.masksToBounds = true
+    }
+    
+    private func setupUserNameLabel() {
+        self.userNameLabel.adjustsFontSizeToFitWidth = true
+        self.userNameLabel.minimumScaleFactor = 0.4
+    }
+    
+    private func setupDeleteUserButton() {
+        self.deleteUserButton.layer.cornerRadius = self.deleteUserButton.frame.width / 2
+        self.deleteUserButton.layer.masksToBounds = true
+    }
+    
+    
+    @IBAction func tapDeleteUserButton(_ sender: Any) {
+        self.deleteUserButtonAction?()
     }
 }

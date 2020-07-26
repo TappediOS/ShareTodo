5b7990082e06299c4aba3930c8c03cec5a87b677
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-07-26 15:02:16 +0900

    変数のセットアップをした

--- ShareTodo/Views/CreateNewGroupInfo/CreateNewGroupInfoViewController.swift
+++ ShareTodo/Views/CreateNewGroupInfo/CreateNewGroupInfoViewController.swift
@@ -12,12 +12,54 @@ final class CreateNewGroupInfoViewController: UIViewController {
     private var presenter: CreateNewGroupInfoViewPresenterProtocol!
     @IBOutlet weak var groupImageView: UIImageView!
     @IBOutlet weak var groupNameTextField: UITextField!
-    @IBOutlet weak var TaskLabel: UILabel!
-    @IBOutlet weak var TaskTextField: UITextField!
+    @IBOutlet weak var taskLabel: UILabel!
+    @IBOutlet weak var taskTextField: UITextField!
     
+    let maxTextfieldLength = 40
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.setupGroupImageView()
+        self.setupGroupNameTextField()
+        self.setupTaskLabel()
+        self.setupTaskTextField()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        self.groupNameTextField.addBorderBottom(borderWidth: 1, color: .systemGray)
+        self.taskTextField.addBorderBottom(borderWidth: 1, color: .systemGray)
+    }
+    
+    func setupGroupImageView() {
+        self.groupImageView.image = UIImage(systemName: "person") ?? UIImage()
+        self.groupImageView.layer.borderWidth = 0.25
+        self.groupImageView.layer.borderColor = UIColor.systemGray4.cgColor
+        self.groupImageView.layer.cornerRadius = self.groupImageView.frame.width / 2
+        self.groupImageView.layer.masksToBounds = true
+    }
+    
+    func setupGroupNameTextField() {
+        self.groupNameTextField.placeholder = "Group Name"
+        self.groupNameTextField.borderStyle = .none
+        self.groupNameTextField.returnKeyType = .done
+        self.groupNameTextField.delegate = self
+        self.groupNameTextField.enablesReturnKeyAutomatically = true
+    }
+    
+    func setupTaskLabel() {
+        self.taskLabel.adjustsFontSizeToFitWidth = true
+        self.taskLabel.minimumScaleFactor = 0.4
+    }
+    
+    func setupTaskTextField() {
+        self.taskTextField.placeholder = "Group Task"
+        self.taskTextField.borderStyle = .none
+        self.taskTextField.returnKeyType = .done
+        self.taskTextField.delegate = self
+        self.taskTextField.enablesReturnKeyAutomatically = true
     }
     
     func inject(with presenter: CreateNewGroupInfoViewPresenterProtocol) {
@@ -28,4 +70,19 @@ final class CreateNewGroupInfoViewController: UIViewController {
 
 extension CreateNewGroupInfoViewController: CreateNewGroupInfoViewPresenterOutput {
     
+}
+
+extension CreateNewGroupInfoViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.groupNameTextField.resignFirstResponder()
+        self.taskTextField.resignFirstResponder()
+        return true
+    }
+    
+    @objc func textFieldDidChange(notification: NSNotification) {
+        guard let textField = notification.object as? UITextField, let text = textField.text else { return }
+        if textField.markedTextRange == nil && text.count > maxTextfieldLength {
+            textField.text = text.prefix(maxTextfieldLength).description
+        }
+    }
 }

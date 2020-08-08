646fa2dde15709c58a32565d280d865e7ede7eb9
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-08-08 12:20:32 +0900

    testGetTodayFormatを追加

--- ShareTodoTests/TodayTodoViewTests.swift
+++ ShareTodoTests/TodayTodoViewTests.swift
@@ -18,7 +18,30 @@ class TodayTodoViewTests: XCTestCase {
     override func tearDownWithError() throws {
         
     }
-
+    
+    func testGetTodayFormat() throws {
+        let todayTodoModel = TodayTodoModel()
+        let exp = todayTodoModel.getTodayFormat()
+        let expArray = exp.components(separatedBy: "_")
+                
+        XCTAssertEqual(expArray.count, 3)
+        XCTAssertEqual(expArray[0].map { String($0) }.count, 4)
+        XCTAssertEqual(expArray[1].map { String($0) }.count, 2)
+        XCTAssertEqual(expArray[2].map { String($0) }.count, 2)
+        
+        let year = Int(expArray[0])!
+        let month = Int(expArray[1])!
+        let day = Int(expArray[2])!
+        
+        XCTAssertGreaterThanOrEqual(year, 2020)
+        
+        XCTAssertGreaterThanOrEqual(month, 1)
+        XCTAssertLessThanOrEqual(month, 12)
+        
+        XCTAssertGreaterThanOrEqual(day, 1)
+        XCTAssertLessThanOrEqual(day, 31)
+    }
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {

68e84226f1a8a38ccb05ccda67200bef489f3232
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-09-02 12:09:21 +0900

    GroupDetail関係のファイルを追加

---
+++ ShareTodo/Views/GroupDetail/GroupDetailViewBuilder.swift
@@ -0,0 +1,9 @@
+//
+//  GroupDetailViewBuilder.swift
+//  ShareTodo
+//
+//  Created by jun on 2020/09/02.
+//  Copyright © 2020 jun. All rights reserved.
+//
+
+import Foundation
---
+++ ShareTodo/Views/GroupDetail/GroupDetailViewController.swift
@@ -0,0 +1,9 @@
+//
+//  GroupDetailViewController.swift
+//  ShareTodo
+//
+//  Created by jun on 2020/09/02.
+//  Copyright © 2020 jun. All rights reserved.
+//
+
+import Foundation

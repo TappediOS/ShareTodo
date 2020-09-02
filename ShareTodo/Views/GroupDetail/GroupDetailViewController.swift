248110c9e634a08cdd74a0081867863a1f15605c
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-09-02 12:28:16 +0900

    groupDetailViewのnavigationTitileを設定

--- ShareTodo/Views/GroupDetail/GroupDetailViewController.swift
+++ ShareTodo/Views/GroupDetail/GroupDetailViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 final class GroupDetailViewController: UIViewController {
     private var presenter: GroupDetailViewPresenterProtocol!
+    var activityIndicator = UIActivityIndicatorView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.setupNavigationBar()
+        self.setupActivityIndicator()
+    }
+    
+    func setupNavigationBar() {
+        self.navigationItem.title = "Group Name"
+        self.navigationItem.largeTitleDisplayMode = .always
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func setupActivityIndicator() {
+        self.activityIndicator.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        self.activityIndicator.center = self.view.center
+        self.activityIndicator.hidesWhenStopped = true
+        self.view.addSubview(self.activityIndicator)
     }
     
     func inject(with presenter: GroupDetailViewPresenterProtocol) {

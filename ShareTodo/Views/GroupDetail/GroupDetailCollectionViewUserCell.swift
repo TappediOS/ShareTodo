45921691786082cfcd096671f834deeba68e07e2
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-09-06 14:30:52 +0900

    groupdetailUsersCellを追加した

---
+++ ShareTodo/Views/GroupDetail/GroupDetailCollectionViewUserCell.swift
@@ -0,0 +1,18 @@
+//
+//  GroupDetailCollectionViewUserCell.swift
+//  ShareTodo
+//
+//  Created by jun on 2020/09/06.
+//  Copyright © 2020 jun. All rights reserved.
+//
+
+import UIKit
+
+class GroupDetailCollectionViewUserCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}

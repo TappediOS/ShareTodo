5f1e647cc8323b840529b7ab0014773730494a81
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-09-23 13:27:17 +0900

    UIのセットアップをかいた

--- ShareTodo/Views/UserDetail/UserDetailViewController.swift
+++ ShareTodo/Views/UserDetail/UserDetailViewController.swift
@@ -15,8 +15,48 @@ final class UserDetailViewController: UIViewController {
     @IBOutlet weak var groupNameLabel: UILabel!
     @IBOutlet weak var groupTaskLabel: UILabel!
     
+    var activityIndicator = UIActivityIndicatorView()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.setupNavigationBar()
+        self.setupActivityIndicator()
+        self.setupGroupImageView()
+        self.setupGroupNameLabel()
+        self.setupGroupTaskLabel()
+    }
+    
+    func setupNavigationBar() {
+        //TODO:- 以下を実際のユーザ名に変更すること
+        self.navigationItem.title = "さんま"
+        self.navigationItem.largeTitleDisplayMode = .always
+        self.navigationController?.navigationBar.tintColor = .systemGreen
+    }
+    
+    func setupGroupImageView() {
+        self.groupImageView.layer.borderWidth = 0.25
+        self.groupImageView.layer.borderColor = UIColor.systemGray4.cgColor
+        self.groupImageView.layer.cornerRadius = self.groupImageView.frame.width / 2
+        self.groupImageView.layer.masksToBounds = true
+    }
+    
+    func setupGroupNameLabel() {
+        self.groupNameLabel.adjustsFontSizeToFitWidth = true
+        self.groupNameLabel.minimumScaleFactor = 0.4
+    }
+    
+    func setupGroupTaskLabel() {
+        self.groupTaskLabel.adjustsFontSizeToFitWidth = true
+        self.groupTaskLabel.minimumScaleFactor = 0.4
+    }
+    
+    func setupActivityIndicator() {
+        self.activityIndicator.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        self.activityIndicator.center = self.view.center
+        self.activityIndicator.hidesWhenStopped = true
+        self.view.addSubview(self.activityIndicator)
     }
     
     func inject(with presenter: UserDetailViewPresenterProtocol) {

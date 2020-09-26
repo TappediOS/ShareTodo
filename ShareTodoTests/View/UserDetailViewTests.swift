f3a1b742cb06b9eea945cbef3bb30d9bbfa72c27
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-09-26 14:00:40 +0900

    viewを閉じたときにprofileImageViewがnavigationBarから削除されていることのテストを追加

--- ShareTodoTests/View/UserDetailViewTests.swift
+++ ShareTodoTests/View/UserDetailViewTests.swift
@@ -40,5 +40,33 @@ class UserDetailViewTests: XCTestCase {
         wait(for: [expectation], timeout: 5)
     }
     
-   
+    func test_viewを閉じたときにprofileImageViewがnavigationBarから削除されていること() {
+        let expectation = self.expectation(description: "expectation")
+        let parentVC = UIViewController()
+        let parentViewController = UINavigationController(rootViewController: parentVC)
+        let presenter = UserDetailViewPresenter(model: UserDetailModelMock())
+        view.inject(with: presenter)
+        
+        let window = UIApplication.shared.connectedScenes.filter({$0.activationState == .foregroundActive}).map({$0 as? UIWindowScene}).compactMap({$0}).first?.windows.filter({$0.isKeyWindow}).first
+        window?.rootViewController = parentViewController
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+            parentVC.navigationController?.pushViewController(self.view, animated: false)
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+                //表示されたときはcontaintsされている
+                XCTAssertTrue(((self.view.navigationController?.navigationBar.subviews.contains(self.view.profileImageView)) != nil))
+                XCTAssertTrue(((parentVC.navigationController?.navigationBar.subviews.contains(self.view.profileImageView)) != nil))
+                self.view.navigationController?.popViewController(animated: false)
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+                    //dismiss後は存在していない
+                    let exp: Bool = (parentVC.navigationController?.navigationBar.subviews.contains(self.view.profileImageView))!
+                    XCTAssertFalse(exp)
+                    expectation.fulfill()
+                })
+            })
+        })
+        
+        wait(for: [expectation], timeout: 3.0)
+        
+    }
 }

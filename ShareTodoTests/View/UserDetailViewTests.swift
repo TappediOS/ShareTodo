0474f42085c492086034d460b1a58646af41f5da
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-09-26 12:35:25 +0900

    UserDetailViewTestsファイルを追加

---
+++ ShareTodoTests/View/UserDetailViewTests.swift
@@ -0,0 +1,21 @@
+//
+//  UserDetailViewTests.swift
+//  ShareTodoTests
+//
+//  Created by jun on 2020/09/26.
+//  Copyright © 2020 jun. All rights reserved.
+//
+
+import XCTest
+@testable import ShareTodo
+
+class UserDetailViewTests: XCTestCase {
+    
+    override func setUpWithError() throws {
+        
+    }
+    
+    override func tearDownWithError() throws {
+        
+    }
+}

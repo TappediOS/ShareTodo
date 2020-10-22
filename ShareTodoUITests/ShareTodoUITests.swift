46caaa2fb2b1dace35c7a53e3eb289b080037042
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-10-22 12:05:30 +0900

    testLaunchPerformanceを追加

--- ShareTodoUITests/ShareTodoUITests.swift
+++ ShareTodoUITests/ShareTodoUITests.swift
@@ -9,6 +9,7 @@
 import XCTest
 
 class ShareTodoUITests: XCTestCase {
+
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
@@ -31,4 +32,12 @@ class ShareTodoUITests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
 }

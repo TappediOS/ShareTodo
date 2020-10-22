0f40cf0703490d760b063fd2a9df9e47738b28ab
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-10-22 12:05:02 +0900

    UITestの起動時パーフォーマンス計測を削除した

--- ShareTodoUITests/ShareTodoUITests.swift
+++ ShareTodoUITests/ShareTodoUITests.swift
@@ -9,7 +9,6 @@
 import XCTest
 
 class ShareTodoUITests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
@@ -32,12 +31,4 @@ class ShareTodoUITests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
 }

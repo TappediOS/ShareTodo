a3a11b76e89279ed426b426029e2537a2d9b3214
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-10-23 11:26:07 +0900

    EditGroupTestsを追加

---
+++ ShareTodoTests/View/EditGroupTests.swift
@@ -0,0 +1,16 @@
+//
+//  EditGroupTests.swift
+//  ShareTodoTests
+//
+//  Created by jun on 2020/10/23.
+//  Copyright © 2020 jun. All rights reserved.
+//
+
+import XCTest
+@testable import ShareTodo
+
+class EditGroupTests: XCTestCase {
+
+    
+    
+}

f14410de352aa7c5ca4dfe17b79abb6b522012eb
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-10-23 11:48:13 +0900

    EditGroupModelTests追加

---
+++ ShareTodoTests/Model/EditGroupModelTests.swift
@@ -0,0 +1,21 @@
+//
+//  EditGroupModelTests.swift
+//  ShareTodoTests
+//
+//  Created by jun on 2020/10/23.
+//  Copyright © 2020 jun. All rights reserved.
+//
+
+class EditGroupModelTests: XCTestCase {
+    
+    override func setUpWithError() throws {
+
+    }
+    
+    override func tearDownWithError() throws {
+
+    }
+
+    
+    
+}

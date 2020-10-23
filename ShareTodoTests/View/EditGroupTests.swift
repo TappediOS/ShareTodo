7900d43c5ec06395478a36a7344bc6d972a8a54f
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-10-23 12:54:23 +0900

    cellをタップしたときのテストを追加

--- ShareTodoTests/View/EditGroupTests.swift
+++ ShareTodoTests/View/EditGroupTests.swift
@@ -15,11 +15,6 @@ class EditGroupTests: XCTestCase {
     override func setUpWithError() throws {
         view = R.storyboard.editGroup().instantiateInitialViewController() as? EditGroupViewController
         guard self.view != nil else { return }
-        
-        let presenter = EditGroupViewPresenter(model: EditGroupModelMock())
-        view.inject(with: presenter)
-        view.loadViewIfNeeded()
-        view.view.layoutIfNeeded()
     }
     
     override func tearDownWithError() throws {
@@ -28,6 +23,10 @@ class EditGroupTests: XCTestCase {
     
     func test_VC表示時にTextFieldの文字が正しいこと() {
         let expectation = self.expectation(description: "expectation")
+        let presenter = EditGroupViewPresenter(model: EditGroupModelMock())
+        view.inject(with: presenter)
+        view.loadViewIfNeeded()
+        view.view.layoutIfNeeded()
         
         DispatchQueue.main.async {
             XCTAssertEqual(self.view.groupNameTextField.text, "Apple")
@@ -39,6 +38,11 @@ class EditGroupTests: XCTestCase {
     }
     
     func test_VC表示時にMembersCollectionViewにMemberが表示されること() {
+        let presenter = EditGroupViewPresenter(model: EditGroupModelMock())
+        view.inject(with: presenter)
+        view.loadViewIfNeeded()
+        view.view.layoutIfNeeded()
+        
         let cell_1 = view.collectionView(view.selectedUsersAndMeCollectionView, cellForItemAt: IndexPath(row: 0, section: 1))
         let cell_2 = view.collectionView(view.selectedUsersAndMeCollectionView, cellForItemAt: IndexPath(row: 1, section: 1))
         let cell_3 = view.collectionView(view.selectedUsersAndMeCollectionView, cellForItemAt: IndexPath(row: 2, section: 1))
@@ -59,5 +63,34 @@ class EditGroupTests: XCTestCase {
         XCTAssertEqual(cell4.nameLabel.text, "user4")
         XCTAssertEqual(cell5.nameLabel.text, "user5")
         XCTAssertEqual(cell6.nameLabel.text, "user6")
+    }
+    
+    func test_collectionViewの自分自身のcellをタップしたとき() {
+        let model = EditGroupModelMock()
+        let presenter = EditGroupViewPresenter(model: model)
+        view.inject(with: presenter)
+        view.loadViewIfNeeded()
+        view.view.layoutIfNeeded()
+        
+        presenter.tapSelectedUsersAndMeProfileImage(index: 0)
+        
+        XCTContext.runActivity(named: "todosの状態が正しいこと") { _ in
+            XCTAssertNil(model.mayRemoveUserUID)
+        }
+    }
+    
+    func test_collectionViewの自分以下のcellをタップしたとき() {
+        let model = EditGroupModelMock()
+        let presenter = EditGroupViewPresenter(model: model)
+        view.inject(with: presenter)
+        view.loadViewIfNeeded()
+        view.view.layoutIfNeeded()
+        
+        for tmp in 1 ..< presenter.groupUsers.count {
+            presenter.tapSelectedUsersAndMeProfileImage(index: tmp)
+            XCTContext.runActivity(named: "todosの状態が正しいこと") { _ in
+                XCTAssertNotNil(model.mayRemoveUserUID)
+            }
+        }
     }
 }

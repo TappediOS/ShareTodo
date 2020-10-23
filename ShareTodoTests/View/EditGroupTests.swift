d2855c2f3fa5fecf66206e4d279e0eba53578ed4
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-10-23 12:32:13 +0900

    test_VC表示時にTextFieldの文字が正しいことを追加

--- ShareTodoTests/View/EditGroupTests.swift
+++ ShareTodoTests/View/EditGroupTests.swift
@@ -10,7 +10,31 @@ import XCTest
 @testable import ShareTodo
 
 class EditGroupTests: XCTestCase {
-
+    var view: EditGroupViewController!
     
+    override func setUpWithError() throws {
+        view = R.storyboard.editGroup().instantiateInitialViewController() as? EditGroupViewController
+        guard self.view != nil else { return }
+        
+    }
+    
+    override func tearDownWithError() throws {
+        view = nil
+    }
+    
+    func test_VC表示時にTextFieldの文字が正しいこと() {
+        let expectation = self.expectation(description: "expectation")
+        let presenter = EditGroupViewPresenter(model: EditGroupModelMock())
+        view.inject(with: presenter)
+        view.loadViewIfNeeded()
+        view.view.layoutIfNeeded()
+        DispatchQueue.main.async {
+            XCTAssertEqual(self.view.groupNameTextField.text, "Apple")
+            XCTAssertEqual(self.view.taskTextField.text, "Pie")
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 5)
+    }
     
 }

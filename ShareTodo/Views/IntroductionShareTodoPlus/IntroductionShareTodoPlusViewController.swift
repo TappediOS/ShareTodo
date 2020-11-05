dafa0e7ac1ec807ad39facbd69001694cb6cc9c2
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-11-05 13:19:48 +0900

    labelをローカライズ対応させた

--- ShareTodo/Views/IntroductionShareTodoPlus/IntroductionShareTodoPlusViewController.swift
+++ ShareTodo/Views/IntroductionShareTodoPlus/IntroductionShareTodoPlusViewController.swift
@@ -11,6 +11,18 @@ import UIKit
 final class IntroductionShareTodoPlusViewController: UIViewController {
     private var presenter: IntroductionShareTodoPlusViewPresenterProtocol!
     @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var premiumFeatureLabel: UILabel!
+    @IBOutlet weak var noAdsImageLabel: UILabel!
+    @IBOutlet weak var checkPastImageLabel: UILabel!
+    @IBOutlet weak var addMessageImageLabel: UILabel!
+    @IBOutlet weak var noAdsLabel: UILabel!
+    @IBOutlet weak var noAdsDescriptionLabel: UILabel!
+    @IBOutlet weak var checkPastLabel: UILabel!
+    @IBOutlet weak var checkPastDescriptionLabel: UILabel!
+    @IBOutlet weak var addMessageLabel: UILabel!
+    @IBOutlet weak var addMessageDescrioptionLabel: UILabel!
+    @IBOutlet weak var applySubscriptionButton: UIButton!
+    @IBOutlet weak var subscriptionNotesLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +30,20 @@ final class IntroductionShareTodoPlusViewController: UIViewController {
         self.setupView()
         self.setupScrollView()
         self.setupNavigationBar()
+        
+        self.setupLabelText()
+        self.setupLabelInfomation(premiumFeatureLabel)
+        self.setupLabelInfomation(noAdsImageLabel)
+        self.setupLabelInfomation(checkPastImageLabel)
+        self.setupLabelInfomation(addMessageImageLabel)
+        self.setupLabelInfomation(noAdsLabel)
+        self.setupLabelInfomation(noAdsDescriptionLabel)
+        self.setupLabelInfomation(checkPastLabel)
+        self.setupLabelInfomation(checkPastDescriptionLabel)
+        self.setupLabelInfomation(addMessageLabel)
+        self.setupLabelInfomation(addMessageDescrioptionLabel)
+        self.setupLabelInfomation(subscriptionNotesLabel)
+        
     }
     
     func setupView() {
@@ -27,6 +53,26 @@ final class IntroductionShareTodoPlusViewController: UIViewController {
     func setupScrollView() {
         self.scrollView.alwaysBounceVertical = true
     }
+    
+    func setupLabelInfomation(_ label: UILabel) {
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.4
+    }
+    
+    func setupLabelText() {
+        premiumFeatureLabel.text         = R.string.localizable.premiumFeatures()
+        noAdsImageLabel.text             = R.string.localizable.noAds()
+        checkPastImageLabel.text         = R.string.localizable.checkPastTasks()
+        addMessageImageLabel.text        = R.string.localizable.addMessageToTask()
+        noAdsLabel.text                  = R.string.localizable.noAdsExMark()
+        noAdsDescriptionLabel.text       = R.string.localizable.noAdsDescription()
+        checkPastLabel.text              = R.string.localizable.checkPastTasks()
+        checkPastDescriptionLabel.text   = R.string.localizable.checkPastTasksDescription()
+        addMessageLabel.text             = R.string.localizable.addMessageToTaskExMark()
+        addMessageDescrioptionLabel.text = R.string.localizable.addMessageToTaskDescription()
+        subscriptionNotesLabel.text      = R.string.localizable.explanationOfSubscriptionNotes()
+    }
+    
     
     func setupNavigationBar() {
         //TODO:- ローカライズ

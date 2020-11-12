4572b15269e84d6735103ba282c1a124275953cf
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-11-12 12:50:39 +0900

    ProfileViewTestにテストを追加した

--- ShareTodoTests/View/ProfileViewTest.swift
+++ ShareTodoTests/View/ProfileViewTest.swift
@@ -12,16 +12,29 @@ import XCTest
 class ProfileViewTest: XCTestCase {
     var view: ProfileViewController!
     
+    weak var delegate: EditProfileViewControllerDelegate?
+    
     override func setUpWithError() throws {
         view = R.storyboard.profile().instantiateInitialViewController() as? ProfileViewController
         guard view != nil else { return }
+        self.delegate = view
     }
 
     override func tearDownWithError() throws {
         
     }
+    
+    func test_起動時にUIの状態が正しいこと() {
+        let presenter = ProfileViewPresenter(model: ProfileViewModelMock())
+        view.inject(with: presenter)
+        
+        view.loadViewIfNeeded()
+        view.view.layoutIfNeeded()
+        XCTAssertTrue(view.scrollView.alwaysBounceVertical)
+        XCTAssertEqual(view.profileImageView.layer.cornerRadius, view.profileImageView.frame.width / 2)
+    }
 
-    func test_showUserNameAndImage() throws {
+    func test_表示時にラベルの情報が正しいこと() throws {
         let presenter = ProfileViewPresenter(model: ProfileViewModelMock())
         view.inject(with: presenter)
         

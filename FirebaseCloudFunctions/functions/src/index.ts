d520afa3015d3b156580ec15ba0af1db3fc3c5d5
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-11-21 16:50:05 +0900

    isFinishedTaskNotificationを追加

--- FirebaseCloudFunctions/functions/src/index.ts
+++ FirebaseCloudFunctions/functions/src/index.ts
@@ -1,9 +1,36 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+admin.initializeApp(functions.config().firebase);
+const firestore = admin.firestore();
+
+
+
+//@ts-ignore TS6133: 'req' is declared but its value is never read.
+async function isFinishedTaskNotification(groupName: string, task: string, finishedUserName: string, fcmToken: string) {
+    const Message = {
+        token: fcmToken,
+        apns: {
+            payload: {
+                aps: {
+                    badge: 0,
+                    headers: {
+                        "apns-priority": "10",
+                    },
+                    //sound: {
+                    //    name: "message_send_009.wav",
+                    //},
+                    alert: {
+                        titleLocKey: "GROUP_TASK_NOTIFICATION_TITLE",
+                        titleLocArgs: [groupName, task],
+                        
+                        locKey: "FINISHEDUSER_NOTIFICATION_BODY",
+                        locArgs: [finishedUserName],
+                    }
+                }
+            },
+        }
+    };
+
+    return admin.messaging().send(Message);
+}

9e72c804a77ca86bf66bab80e62fde27f23c428d
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-11-23 13:13:19 +0900

    userCollection読み取りテストを書いた

--- FirebaseCloudFunctions/test/test.js
+++ FirebaseCloudFunctions/test/test.js
@@ -24,9 +24,17 @@ describe("user collectionに対するUnitTest", () => {
         await firebase.assertSucceeds(myDoc.get());
     });
 
-    it("他人のデータを読み取ることができない", async () => {
+    it("認証していれば，他人のデータを読み取ることができる", async () => {
         const db = getFirestore(myAuth);
-        const myDoc = db.collection("todo/v1/users").doc(otherId);
+        const otherDoc = db.collection("todo/v1/users").doc(otherId);
+        await firebase.assertSucceeds(otherDoc.get());
+    });
+
+    it("認証していなければ，どのデータも読み取ることはできない", async () => {
+        const db = getFirestore(null);
+        const myDoc = db.collection("todo/v1/users").doc(myId);
+        const otherDoc = db.collection("todo/v1/users").doc(otherId);
         await firebase.assertFails(myDoc.get());
+        await firebase.assertFails(otherDoc.get());
     });
 });

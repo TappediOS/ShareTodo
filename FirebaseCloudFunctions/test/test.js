cf7bcd5a962f0a012a7352b4c75cb06d73868422
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-11-23 13:08:05 +0900

    userData読み取りに対するテストを追加した

--- FirebaseCloudFunctions/test/test.js
+++ FirebaseCloudFunctions/test/test.js
@@ -1,16 +1,32 @@
 const assert = require('assert');
-
 const firebase = require('@firebase/rules-unit-testing');
+const MY_PROJECT_ID = "sharetodo-a91ef";
+const serverTimestamp = () => firebase.firestore.FieldValue.serverTimestamp();
 
-const MY_PROJECT_ID = "";
-
-const myID = "user_me";
-const otherID = "user_other";
+const myId = "user_me";
+const otherId = "user_other";
 
 const myAuth = { uid: myId, };
 
-describe("UnitTest", () => {
-    it("Add", () => {
-        assert.strictEqual(2+2, 4);
+function getFirestore(auth) {
+    return firebase.initializeTestApp( {projectId: MY_PROJECT_ID, auth: auth}).firestore();
+}
+
+beforeEach(async () => {
+    await firebase.clearFirestoreData({projectId: MY_PROJECT_ID});
+});
+
+
+describe("user collectionに対するUnitTest", () => {
+    it("自分のデータを読み取ることができる", async () => {
+        const db = getFirestore(myAuth);
+        const myDoc = db.collection("todo/v1/users").doc(myId);
+        await firebase.assertSucceeds(myDoc.get());
+    });
+
+    it("他人のデータを読み取ることができない", async () => {
+        const db = getFirestore(myAuth);
+        const myDoc = db.collection("todo/v1/users").doc(otherId);
+        await firebase.assertFails(myDoc.get());
     });
 });

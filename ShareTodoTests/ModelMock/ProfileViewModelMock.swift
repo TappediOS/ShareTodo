94ceb2e8de88162db9c22dd50dcecad6943167fc
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-11-30 17:44:11 +0900

    ProfileModelMockを修正した

--- ShareTodoTests/ModelMock/ProfileViewModelMock.swift
+++ ShareTodoTests/ModelMock/ProfileViewModelMock.swift
@@ -7,12 +7,29 @@
 //
 
 @testable import ShareTodo
+import Foundation
 
 class ProfileViewModelMock: ProfileModelProtocol {
     var presenter: ProfileModelOutput!
+    var isUserSubscribed: Bool = false
     
     func fetchUser() {
         let result = User(id: "123", name: "Rock", profileImageURL: nil)
         self.presenter.successFetchUser(user: result)
+    }
+    
+    func checkingIfAUserSubscribed() {
+        self.isUserSubscribed = true
+        self.presenter.userSubscribed()
+    }
+    
+    @objc func startSubscribed() {
+        self.isUserSubscribed = true
+        self.presenter.userStartSubscribed()
+    }
+    
+    @objc func endSubscribed() {
+        self.isUserSubscribed = false
+        self.presenter.userEndSubscribed()
     }
 }

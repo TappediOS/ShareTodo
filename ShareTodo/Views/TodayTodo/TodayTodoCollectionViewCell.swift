d7e3bde6a4b1e86a8dec04db5f0b37cb6c92fff9
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2021-02-18 22:37:32 +0900

    アニメーションを追加

--- ShareTodo/Views/TodayTodo/TodayTodoCollectionViewCell.swift
+++ ShareTodo/Views/TodayTodo/TodayTodoCollectionViewCell.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import Nuke
+import Lottie
+import Sica
 
 class TodayTodoCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var groupImageView: UIImageView!
@@ -20,11 +22,14 @@ class TodayTodoCollectionViewCell: UICollectionViewCell {
     var radioButtonAction: (() -> Void)?
     var writeMessageButtonAction: (() -> Void)?
     
+    private var isFinishedTodo = false
+    private var isSubscribed = false
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         self.backgroundColor = .systemBackground
         self.layer.cornerRadius = 10
-        self.clipsToBounds = true
+        self.layer.masksToBounds = false  // animationが見切れないため
         
         self.setupGroupImageView()
         self.setupTaskLabel()
@@ -46,7 +51,7 @@ class TodayTodoCollectionViewCell: UICollectionViewCell {
     
     func setupRadioButton() {
         self.radioButton.layer.cornerRadius = self.groupImageView.frame.width / 2
-        self.radioButton.layer.masksToBounds = true
+        self.radioButton.layer.masksToBounds = false
         self.radioButton.tintColor = .systemGreen
     }
     
@@ -57,6 +62,9 @@ class TodayTodoCollectionViewCell: UICollectionViewCell {
     }
     
     func configure(with group: Group, isFinished: Bool, isWrittenMessage: Bool, isSubscribed: Bool) {
+        self.isFinishedTodo = isFinished
+        self.isSubscribed = isSubscribed
+        
         let radioButtonImage = isFinished ? UIImage(systemName: "checkmark.circle.fill") : UIImage(systemName: "checkmark.circle")
         let writeMessageButtonImage = isWrittenMessage ? UIImage(systemName: "pencil.circle.fill") : UIImage(systemName: "pencil.circle")
         
@@ -80,11 +88,102 @@ class TodayTodoCollectionViewCell: UICollectionViewCell {
         }
     }
     
+    private func changeRadioButtonImage() {
+        let radioButtonImage = self.isFinishedTodo ? UIImage(systemName: "checkmark.circle.fill") : UIImage(systemName: "checkmark.circle")
+        self.radioButton.setImage(radioButtonImage, for: .normal)
+        self.writeMessageButton.setImage(UIImage(systemName: "pencil.circle"), for: .normal)
+    }
+    
     @IBAction func tapRadioButton(_ sender: Any) {
         self.radioButtonAction?()
+        
+        self.isFinishedTodo.toggle()
+        self.changeRadioButtonImage()
+        if self.isSubscribed { self.writeMessageButton.isHidden = !self.isFinishedTodo }
+        // toggleの後がfalseの時，つまり，finishをunFinishにする際はanimationはしないのでreturnする
+        guard self.isFinishedTodo == true else { return }
+        
+        self.playRadioButtonSpringAnimation() //ボタンが跳ねるアニメーション
+        
+        
+        print(ShareTodoRemoteConfig.finishTodoAnimationValue ?? "nil")
+        switch ShareTodoRemoteConfig.finishTodoAnimationValue {
+        case R.string.sharedString.remoteConfig_star(): playStarAnimation()
+        case R.string.sharedString.remoteConfig_mudRaibow(): playMudRainbowAnimation()
+        case R.string.sharedString.remoteConfig_fireworks(): playFireworksAnimation()
+        case R.string.sharedString.remoteConfig_cracker(): playCrackerAnimation()
+        case R.string.sharedString.remoteConfig_spark(): playSparkAnimation()
+        case R.string.sharedString.remoteConfig_randomAnimation(): playRandomAnimation()
+        default: playStarAnimation() // nilの場合とdefaultの時はここ
+        }
     }
     
     @IBAction func tapWriteMessageButton(_ sender: Any) {
         self.writeMessageButtonAction?()
+    }
+    
+    
+    private func playRadioButtonSpringAnimation() {
+        guard let imageView = self.radioButton.imageView else { return }
+        let sprintAnimation = Animator(view: self.radioButton.imageView!)
+        sprintAnimation.addSpringAnimation(keyPath: .boundsSize, from: CGSize(width: 0, height: 0), to: imageView.frame.size, damping: 10, mass: 1, stiffness: 200, initialVelocity: 0, duration: 1, delay: 0).run(type: .sequence)
+    }
+    
+    // MARK: - アニメーション関数
+    
+    private func playStarAnimation() {
+        guard let imageFrame = self.radioButton.imageView?.frame else { return }
+        let starAnimationView = AnimationView().getStarAnimationView(stackViewFrame: buttonStackView.frame, targetViewFrame: imageFrame)
+        self.addSubview(starAnimationView)
+        starAnimationView.play { finished in
+            if finished { starAnimationView.removeFromSuperview() }
+        }
+    }
+    
+    private func playCrackerAnimation() {
+        guard let imageFrame = self.radioButton.imageView?.frame else { return }
+        let starAnimationView = AnimationView().getCrackerAnimationView(stackViewFrame: buttonStackView.frame, targetViewFrame: imageFrame)
+        self.addSubview(starAnimationView)
+        starAnimationView.play { finished in
+            if finished { starAnimationView.removeFromSuperview() }
+        }
+    }
+    
+    private func playFireworksAnimation() {
+        guard let imageFrame = self.radioButton.imageView?.frame else { return }
+        let starAnimationView = AnimationView().getFireworksAnimationView(stackViewFrame: buttonStackView.frame, targetViewFrame: imageFrame)
+        self.addSubview(starAnimationView)
+        starAnimationView.play { finished in
+            if finished { starAnimationView.removeFromSuperview() }
+        }
+    }
+    
+    private func playMudRainbowAnimation() {
+        guard let imageFrame = self.radioButton.imageView?.frame else { return }
+        let starAnimationView = AnimationView().getMudRainbowAnimationView(stackViewFrame: buttonStackView.frame, targetViewFrame: imageFrame)
+        self.addSubview(starAnimationView)
+        starAnimationView.play { finished in
+            if finished { starAnimationView.removeFromSuperview() }
+        }
+    }
+    
+    private func playSparkAnimation() {
+        guard let imageFrame = self.radioButton.imageView?.frame else { return }
+        let starAnimationView = AnimationView().getSparkAnimationView(stackViewFrame: buttonStackView.frame, targetViewFrame: imageFrame)
+        self.addSubview(starAnimationView)
+        starAnimationView.play { finished in
+            if finished { starAnimationView.removeFromSuperview() }
+        }
+    }
+    
+    private func playRandomAnimation() {
+        switch Int.random(in: 1...5) {
+        case 1: playStarAnimation()
+        case 2: playCrackerAnimation()
+        case 3: playFireworksAnimation()
+        case 4: playMudRainbowAnimation()
+        case 5: playSparkAnimation()
+        default: playStarAnimation()
+        }
     }
 }

b8cd2db56a73ee4195c1f4904ecfff14c03343ec
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2021-03-07 13:02:56 +0900

    OnBoarding画面でトラッキングのプロンプトを出す様にした

--- ShareTodo/Views/OnBoarding/OnBoardingViewModel.swift
+++ ShareTodo/Views/OnBoarding/OnBoardingViewModel.swift
@@ -6,8 +6,14 @@
 //  Copyright © 2020 jun. All rights reserved.
 //
 
+import Firebase
+import AppTrackingTransparency
+import AdSupport
+
 protocol OnBoardingModelProtocol {
     var presenter: OnBoardingModelOutput! { get set }
+
+    func requestAdsTrackingIfNeeded()
 }
 
 protocol OnBoardingModelOutput: class {
@@ -16,4 +22,22 @@ protocol OnBoardingModelOutput: class {
 
 final class OnBoardingModel: OnBoardingModelProtocol {
     weak var presenter: OnBoardingModelOutput!
+
+
+    func requestAdsTrackingIfNeeded() {
+        if #available(iOS 14, *) {
+            if ATTrackingManager.trackingAuthorizationStatus == .notDetermined {
+                ATTrackingManager.requestTrackingAuthorization(completionHandler: { status in
+                    switch status {
+                    case .authorized: Analytics.logEvent(R.string.sharedString.adsTrackingAuthorized_EventName(), parameters: nil)
+                    case .denied: Analytics.logEvent(R.string.sharedString.adsTrackingDenied_EventName(), parameters: nil)
+                    case .restricted, .notDetermined: return
+                    @unknown default: return
+                    }
+                })
+            }
+        } else {
+            return
+        }
+    }
 }
